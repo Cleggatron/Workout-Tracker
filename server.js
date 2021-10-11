3069a69aa22ed01aadaf2a04b19aa3eba0ae8d01
Cleggatron <david.robert.clegg@gmail.com>
2021-10-11 21:45:52 +0100

    added our middlewares

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require("express");
 const PORT = proces.env.port || 3001;
 const app = express();
 
+//set up our middle wares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, "public")))
+
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
 })
