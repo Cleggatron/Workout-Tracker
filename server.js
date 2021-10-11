44e32259b5f53f23d9f0757e849306e25fee816c
Cleggatron <david.robert.clegg@gmail.com>
2021-10-11 21:47:13 +0100

    added base of routing

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 
+//import our routing files
+const routers = require("./controllers");
+
 const PORT = proces.env.port || 3001;
 const app = express();
 
@@ -7,6 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, "public")))
+
+//set up routing
+app.use(routers)
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
