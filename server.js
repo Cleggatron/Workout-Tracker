89358b1e6e95ea109116a02a6bffa660d399f79d
Cleggatron <david.robert.clegg@gmail.com>
2021-10-12 22:48:14 +0100

    added mongoose to server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 
 const PORT = process.env.port || 3001;
 const app = express();
@@ -9,6 +9,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 //set up routing
+app.get("/", function(req, res){
+    res.sendFile("index.html")
+})
+
+app.get("/exercise", function(req, res){
+    res.sendFile("exercise.html")
+})
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
