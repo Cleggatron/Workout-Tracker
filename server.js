e4743cd5b259e970d079442b21118c8738259e86
Cleggatron <david.robert.clegg@gmail.com>
2021-10-15 20:25:03 +0100

    added basic routing

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const PORT = process.env.port || 3001;
 const app = express();
 
+const router = require("./controllers")
+
 //set up our middle wares
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -13,6 +15,8 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
   useNewUrlParser: true
 });
+
+app.use(router)
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
