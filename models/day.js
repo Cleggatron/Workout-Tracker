385e2dc8f0b1387a433d0f1e256a7f7d5d08ea37
Cleggatron <david.robert.clegg@gmail.com>
2021-10-15 19:49:34 +0100

    wrote day schema

---
+++ models/day.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const daySchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Exercise"
+        }
+    ]
+});
+
+const Day = mongoose.model("Day", daySchema)
+module.exports = Day;
---
+++ models/index.js
@@ -0,0 +1,4 @@
+const Day = require("./day.js");
+const Exercise = require("./exercise.js")
+
+module.exports = {Day, Exercise};
