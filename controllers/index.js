c9f58ba4e112b01028f95b5c9687cb63913cf426
Cleggatron <david.robert.clegg@gmail.com>
2021-10-15 20:29:57 +0100

    set up basic routing paths

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,8 @@
 const router = require("express").Router()
+const exerciseRoutes = require("./exercise");
+const statsRoutes = require("./stats");
 
+router.use("/exercise", exerciseRoutes)
+router.use("/stats", statsRoutes)
 
 module.exports = router
