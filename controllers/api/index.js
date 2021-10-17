bd39916de668e3bc57a34a4b43e5ec7dce387458
Cleggatron <david.robert.clegg@gmail.com>
2021-10-17 09:12:41 +0100

    wrote first get and post actions

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,31 @@
 const router = require("express").Router()
 const { Workout } = require("../../models");
 
-router.get("/workouts", async (req, res) => {
-    Workout.find({}).then(data => {
-        console.log(data)
-        res.status(200).end()
+router.get("/workouts", (req, res) => {
+    Workout.aggregate(
+        [
+            {
+                $addFields: {
+                    totalDuration: {$sum: "$exercises.duration"}
+                }
+            }
+        ]
+    ).then(data => {
+        res.status(200).json(data)
     }).catch(err => {
         res.status(400).json(err)
     })
 })
+
+router.post("/workouts", async (req, res) => {
+    console.log(req.body);
+    Workout.create(req.body).then(data => {
+        res.json(data)
+    }).catch(err => {
+        res.status(400).json(err)
+    })
+})
+
+router.put("/")
 
 module.exports = router;
